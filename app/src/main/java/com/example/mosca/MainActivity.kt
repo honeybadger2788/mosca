c2aeea3ed30d31e0a551349045e3855e897a47a0
honeybadger2788 <noe.carosella@gmail.com>
2022-09-22 14:37:05 -0300

    add btn listeners

--- app/src/main/java/com/example/mosca/MainActivity.kt
+++ app/src/main/java/com/example/mosca/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.mosca
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.mosca.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -11,5 +12,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener{
+            if(binding.etUsername.text.toString().isNotBlank() && binding.etPassword.text.toString().isNotBlank()){
+                showMessage("Hola, ${binding.etUsername.text.toString()}")
+            } else {
+                showMessage("Algo salió mal")
+            }
+        }
+
+        binding.btnRegister.setOnClickListener{
+            showMessage("Hora de registrarse")
+        }
+    }
+
+    private fun showMessage(msg: String){
+        Snackbar.make(binding.root, msg, Snackbar.LENGTH_SHORT)
+            .setAnchorView(binding.llLogin)
+            .show()
     }
 }
