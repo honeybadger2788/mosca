4f593dd72f2e6a6f75d2318fa559718d280f34f6
honeybadger2788 <noe.carosella@gmail.com>
2022-11-07 15:09:16 -0300

    add firebase authentication at RegisterActivity

--- app/src/main/java/com/example/mosca/RegisterActivity.kt
+++ app/src/main/java/com/example/mosca/RegisterActivity.kt
@@ -1,20 +1,55 @@
 package com.example.mosca
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mosca.databinding.ActivityRegisterBinding
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class RegisterActivity: AppCompatActivity() {
     private lateinit var binding: ActivityRegisterBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        auth = Firebase.auth
+
         /* Habilita la opción de VOLVER */
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        registerNewAccount()
+    }
+
+    private fun registerNewAccount() {
+        with(binding){
+            btnRegister.setOnClickListener {
+                if (etPassword.text.toString() == etConfirmPassword.text.toString()){
+                    if (etEmail.text!!.isNotBlank()
+                        && etPassword.text!!.isNotBlank()) {
+                        val email = etEmail.text.toString()
+                        val password = etPassword.text.toString()
+
+                        auth.createUserWithEmailAndPassword(email, password)
+                            .addOnCompleteListener { task ->
+                                if (task.isSuccessful) {
+                                    startActivity(Intent(this@RegisterActivity, AuthActivity::class.java))
+                                } else {
+                                    showMessage("Algo salió mal")
+                                }
+                            }
+                    }
+                } else {
+                    showMessage("Las contraseñas no coinciden")
+                }
+            }
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -22,5 +57,9 @@ class RegisterActivity: AppCompatActivity() {
             android.R.id.home -> onBackPressed()
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun showMessage(msg: String){
+        Snackbar.make(binding.root, msg, Snackbar.LENGTH_SHORT).show()
     }
 }
